a344c369fa61f06163dca168c5035828b4ab96f0
lohith0815 <147791677+lohith0815@users.noreply.github.com>
2023-12-08 10:51:14 +0530

    #include<stdio.h> int main() {     int a,b,c,cost;     scanf("%d%d%d",&a,&b,&c);     cost=(2*(a+b))*c;     printf("%d",cost); }

--- Frames.c
+++ Frames.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int n,m,x,cost;
-    scanf("%d%d%d",&n,&m,&x);
-    cost=(2*(n+m))*x;
+    int a,b,c,cost;
+    scanf("%d%d%d",&a,&b,&c);
+    cost=(2*(a+b))*c;
     printf("%d",cost);
 }
