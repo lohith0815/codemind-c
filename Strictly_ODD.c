fc1a4cd9e3660b2d7566ab414b29ed80bbf3d3a1
lohith0815 <147791677+lohith0815@users.noreply.github.com>
2023-12-16 11:35:57 +0530

    #include <stdio.h> int main() {     int n, i, a[100], is_strictly_odd = 1;     scanf("%d", &n);     for (i = 0; i < n; i++)     {         scanf("%d", &a[i]);         if (a[i] % 2 != 0 && i % 2 == 0)         {             is_strictly_odd = 0;         }     }     if (is_strictly_odd)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Strictly_ODD.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, a[100], is_strictly_odd = 1;
+    scanf("%d", &n);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+        if (a[i] % 2 != 0 && i % 2 == 0)
+        {
+            is_strictly_odd = 0;
+        }
+    }
+    if (is_strictly_odd)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
